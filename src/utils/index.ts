84adbc9dd3c99c5777eb57d373d2141b2c9b2fa3
Mallen220 <allenmc220@gmail.com>
2025-12-03 14:20:08 -0500

    Reorder exports in utils index file
    Updated the export order in src/utils/index.ts for improved organization and consistency. No functional changes were made.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,9 @@
-export * from "./draw";
-export * from "./math";
-export * from "./codeExporter";
-export * from "./geometry";
-export * from "./file";
 export * from "./animation";
+export * from "./codeExporter";
+export * from "./draw";
+export * from "./file";
+export * from "./geometry";
+export * from "./math";
 export * from "./shapes";
 export * from "./timeCalculator";
 
