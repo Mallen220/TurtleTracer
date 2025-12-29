655fce46aef3ca8c81e91f13498043d27315f608
Mallen220 <allenmc220@gmail.com>
2025-12-29 02:44:41 +0000

    Standardize delete buttons to use trash can icon
    Updated various components (EventMarkersSection, ObstaclesSection, WaitMarkersSection, ControlPointsSection, PathLineSection, WaitRow) to use the standardized `TrashIcon` component instead of inline SVGs or other icons.
    
    Also updated `WaypointTable.svelte` to correctly use `class_` prop for `TrashIcon` instead of `className`.

--- src/lib/components/PathLineSection.svelte
+++ src/lib/components/PathLineSection.svelte
@@ -6,6 +6,7 @@
   import HeadingControls from "./HeadingControls.svelte";
   import ColorPicker from "./ColorPicker.svelte";
   import { selectedLineId } from "../../stores";
+  import TrashIcon from "./icons/TrashIcon.svelte";
 
   export let line: Line;
   export let idx: number;
@@ -237,20 +238,12 @@
       </button>
 
       {#if lines.length > 1}
-        <button title="Remove Line" on:click={onRemove}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width={2}
-            class="size-5 stroke-red-500"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
+        <button
+          title="Remove Line"
+          class="text-red-500 hover:text-red-600"
+          on:click={onRemove}
+        >
+          <TrashIcon class_="size-5" strokeWidth={2} />
         </button>
       {/if}
     </div>
