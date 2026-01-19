2a18726a5a41ba13acf18f7bdbad274bc6fc1fbe
Mallen220 <allenmc220@gmail.com>
2026-01-19 18:53:12 -0500

    Update pedro.d.ts

--- plugins/pedro.d.ts
+++ plugins/pedro.d.ts
@@ -1,4 +1,5 @@
 // Copyright 2026 Matthew Allen. Licensed under the Apache License, Version 2.0.
+
 /**
  * Type definitions for Pedro Pathing Visualizer Plugins.
  * These types are automatically available in your .ts plugins.
@@ -103,7 +104,10 @@ type SequenceRotateItem = {
   _linkedName?: string; // Metadata for linked names
 };
 
-type SequenceItem = SequencePathItem | SequenceWaitItem | SequenceRotateItem;
+type SequenceItem =
+  | SequencePathItem
+  | SequenceWaitItem
+  | SequenceRotateItem;
 
 interface KeyBinding {
   id: string;
