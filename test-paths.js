5f8dc573961c10416ba005958b4552208e158dbb
Mallen220 <allenmc220@gmail.com>
2026-02-28 10:39:43 -0500

    format

--- test-paths.js
+++ test-paths.js
@@ -1,3 +1,4 @@
+// Copyright 2026 Matthew Allen. Licensed under the Modified Apache License, Version 2.0.
 const path = require("path");
 const p =
   "C:\\Users\\user\\AppData\\Local\\Packages\\PedroPathing\\LocalCache\\Roaming\\pedro";
