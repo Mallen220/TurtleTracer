37f2584519603dc9c6f11b91c5ef2204de01ef5b
Mallen220 <allenmc220@gmail.com>
2026-03-24 13:54:44 -0400

    Delete test.cjs

--- test.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/lib/components/dialogs/ExportImageDialog.svelte', 'utf-8');
-const lines = code.split('\n');
-console.log("Lines around downloadImage implementation:");
-for(let i=122; i<132; i++) {
-  console.log(i+1, lines[i]);
-}
