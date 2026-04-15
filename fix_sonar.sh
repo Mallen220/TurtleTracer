3a370c3196b3382af2c67ec72c6b4fc294c3392d
Mallen220 <68817732+Mallen220@users.noreply.github.com>
2026-04-15 19:28:13 +0000

    Remove test file generating sonar warning
    - Removed fix_console_pollution scripts that caused sonarqube failures
    - Reverted to simple vitest.config.ts setup

--- fix_sonar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Remove console pollution tests as SonarQube complains about mockRestore not working properly or missing mocks for console
-# We will just remove the specific sonarqube issues by rewriting updater.test.js without console mocks
